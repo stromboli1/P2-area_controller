8653fbc94ffd85fdd0637e1b28ab00eb44969f41
ReturnOfTheLast <returnofthelast@gmail.com>
2023-04-26 13:52:05 +0200

    Autopopulation of houses

--- initdb.py
+++ initdb.py
@@ -1,6 +1,16 @@
-from models import Base
+from sqlalchemy.orm import sessionmaker
+from models import Base, HousePool
 from utils import engine
 
 
 if __name__ == "__main__":
     Base.metadata.create_all(engine)
+
+    houses = []
+    for i in range(3):
+        houses.append(HousePool(name = f"House {i+1}", ip = f"10.10.0.10{i+1}"))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.add_all(houses)
+    session.commit()
