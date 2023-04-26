e1fa318a53d6821a79c369723ebab3a5ba29c97c
TheEngbjerg <nicolaiengbjerg@gmail.com>
2023-04-26 13:21:03 +0200

    added database sorting

--- recv_unpack.py
+++ recv_unpack.py
@@ -1,14 +1,15 @@
 #module import
 import socket
-import models
 from sqlalchemy.orm import sessionmaker
+from utils import engine
+from models import HousePool, HDData
 
 #variables
 PORT = 42070
-ADDR = "0.0.0.0"
+ADDR = ""
 
 #setup database session
-Session = sessionmaker()
+Session = sessionmaker(bind = engine)
 session = Session()
 
 #setup socket udp server
@@ -18,6 +19,22 @@ soc.bind((ADDR, PORT))
 #listen for incoming messages
 while True:
     byte_message = soc.recvfrom(1024)
+    data, house_addr = byte_message
 
-    data = byte_message[0]
-    house_ip = byte_message[1]
+    device_state = data[0]
+    power_usage = data[1:5]
+    temperature = data[5:9]
+    unix_timestamp = data[9:]
+
+    house_id = session.query(HousePool).filter(
+            HousePool.ip == house_addr[0]
+            ).first().id
+
+    entry = HDData(device_state = device_state,
+                   power_usage = power_usage,
+                   temperature = temperature,
+                   timestamp = unix_timestamp,
+                   house_id = house_id)
+
+    session.add(entry)
+    session.commit()
