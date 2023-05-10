38b095e3a04307452289d7fcc441ee2d0bb2d56f
TheEngbjerg <nicolaiengbjerg@gmail.com>
2023-05-10 14:10:26 +0200

    fixed adress

--- start_protocol.py
+++ start_protocol.py
@@ -1,14 +1,23 @@
 # imports
 import socket
+from models import HousePool
+from sqlalchemy.orm import sessionmaker
+from utils import engine
+
+# create session
+Session = sessionmaker(bind = engine)
+session = Session()
 
 def start_houses() -> None:
 
     start_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    start_sock.sendto(b'\x01', 6969)
+    for house in session.query(HousePool):
+        start_sock.sendto(b'\x01', (house.ip, 6969))
     start_sock.close()
 
 def stop_houses() -> None:
 
     start_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    start_sock.sendto(b'\x00', 6969)
+    for house in session.query(HousePool):
+        start_sock.sendto(b'\x00', (house.ip, 6969))
     start_sock.close()
