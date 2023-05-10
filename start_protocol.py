1b572164a8f93137f746581a04a3e89eb73a779a
TheEngbjerg <nicolaiengbjerg@gmail.com>
2023-05-10 14:05:44 +0200

    added port

--- start_protocol.py
+++ start_protocol.py
@@ -4,7 +4,7 @@ import socket
 def start_houses() -> None:
 
     start_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    start_sock.sendto(b'\x01', )
+    start_sock.sendto(b'\x01', 6969)
     start_sock.close()
 
 def stop_houses() -> None:
