39f708cb010a4ef124e10ff80fa36a00db81e20d
Musvon2705 <Musvon2705@gmail.com>
2023-05-11 09:21:41 +0200

    Change address in broadcast

--- start_protocol.py
+++ start_protocol.py
@@ -6,5 +6,5 @@ def onoff_houses() -> None:
     start_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     start_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     start_sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    start_sock.sendto(b'\x01', ('255.255.255.255', 6969))
+    start_sock.sendto(b'\x01', ('<broadcast>', 6969))
     start_sock.close()
