248257356124853b6c34def9fb664a01e52d848e
Musvon2705 <Musvon2705@gmail.com>
2023-05-11 09:54:50 +0200

    update address in start sock

--- start_protocol.py
+++ start_protocol.py
@@ -7,7 +7,7 @@ def onoff_houses(on_off = False) -> None:
     start_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     start_sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     if on_off:
-        start_sock.sendto(b'\x01', ('10.10.0.101', 6969))
+        start_sock.sendto(b'\x01', ('10.10.0.255', 6969))
         start_sock.close()
     elif not on_off:
         start_sock.sendto(b'\x00', ('<broadcast>', 6969))
