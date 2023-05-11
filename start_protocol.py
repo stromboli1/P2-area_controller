5894412ac74ae9a9b28ba358b654dc7c542895b4
Musvon2705 <Musvon2705@gmail.com>
2023-05-11 09:19:09 +0200

    Minor fix to start protocol

--- start_protocol.py
+++ start_protocol.py
@@ -4,6 +4,7 @@ import socket
 def onoff_houses() -> None:
 
     start_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    start_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     start_sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     start_sock.sendto(b'\x01', ('255.255.255.255', 6969))
     start_sock.close()
