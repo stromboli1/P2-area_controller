ce6e36dce60384840cdb9cec9fd951ec965ca911
Musvon2705 <Musvon2705@gmail.com>
2023-05-15 13:57:40 +0200

    Use of adaptive naming of graph

--- graph_dev.py
+++ graph_dev.py
@@ -47,7 +47,6 @@ def live_graph():
     plt.xlabel("Seconds")
 
     if len(x) > 1:
-        if x[-1] // 600 >=1 and x[-1] % 600 == 0:
-            name = f"graph_file_{x[-1]//600}.svg"
-            #name = "graph_file.svg"
+        if x[-1] // 86400 >=1 and x[-1] % 86400 == 0:
+            name = f"graph_file_{x[-1]//86400}.svg"
             plt.savefig(name)
